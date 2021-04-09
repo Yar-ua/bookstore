6aa09f7e9c3d54cddf4f4df476ebacdd5354e128
yar-ua <yar.ua.1985@gmail.com>
2021-04-09 22:12:18 +0300

    add some tests

--- spec/views/home/home.html.haml_spec.rb
+++ spec/views/home/home.html.haml_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'home/home.html.haml', type: :view do
   describe 'hame page' do
-    it 'aaa' do
-      render template: 'home/home'
-      expect(rendered).to include('Welcome to our amazing Bookstore!')
-    end
+    before { render template: 'home/home' }
+
+    it { expect(rendered).to have_content('Welcome to our amazing Bookstore!') }
+    it { expect(rendered).to have_content('Best Sellers') }
+    it { expect(rendered).to have_selector('#slider') }
+    it { expect(rendered).to have_button('Buy Now') }
   end
 end
