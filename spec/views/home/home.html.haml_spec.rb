d3a94ac9076f7f5616c4a0ed9652be64178c00c9
yar-ua <yar.ua.1985@gmail.com>
2021-04-10 19:58:04 +0300

    update feature tests

--- spec/views/home/home.html.haml_spec.rb
+++ spec/views/home/home.html.haml_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe 'home/home.html.haml', type: :view do
   describe 'hame page' do
     before { render template: 'home/home' }
 
-    it { expect(rendered).to have_content('Welcome to our amazing Bookstore!') }
-    it { expect(rendered).to have_content('Best Sellers') }
-    it { expect(rendered).to have_selector('#slider') }
-    it { expect(rendered).to have_button('Buy Now') }
+    describe 'have slider' do
+      it { expect(rendered).to have_selector('.slider-item', maximum: 3) }
+      it { expect(rendered).to have_button('Buy Now') }
+    end
+    
+    describe 'have text content' do
+      it { expect(rendered).to have_content('Welcome to our amazing Bookstore!') }
+      it { expect(rendered).to have_button('Get Started') }
+    end
+    
+    describe 'have cards' do
+      it { expect(rendered).to have_content('Best Sellers') }
+      it { expect(rendered).to have_selector('.best-sellers', count: 4) }
+    end
   end
 end
