e23b75025f399c0b07918e8518afff0d911826e4
yar-ua <yar.ua.1985@gmail.com>
2021-04-15 21:55:55 +0300

    switch to controller  tests

--- spec/controllers/home_spec.rb
+++ spec/controllers/home_spec.rb
@@ -1,8 +1,12 @@
-RSpec.describe 'Home', type: :request do
+RSpec.describe HomeController do
   describe 'GET /home' do
+    before { get :index }
     it 'returns http success' do
-      get root_path
       expect(response).to have_http_status(:success)
+    end
+    
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
     end
   end
 end
