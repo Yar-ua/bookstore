4c3def79d38fb73309d9fae7faee340c9fd7a6ac
yar-ua <yar.ua.1985@gmail.com>
2021-04-16 23:24:40 +0300

    add book author category models and views

---
+++ app/models/book.rb
@@ -0,0 +1,8 @@
+class Book < ApplicationRecord
+  has_many :authorships, dependent: :destroy
+  has_many :authors, through: :authorships, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :price, presence: true, numericality: true
+  validates :height, :width, :depth, numericality: true
+end
