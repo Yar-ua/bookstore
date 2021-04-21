25f76f39a4d5f591dedbbdb5897b7eeed29a6661
yar-ua <yar.ua.1985@gmail.com>
2021-04-21 20:12:22 +0300

    fix ci

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,6 @@
 module BooksHelper
   VISIBLE_LENGTH_DESCRIPTION_BOOK = 100
-  
+
   def book_authors(book)
     book.authors.map { |author| "#{author.first_name} #{author.last_name}" }.join(', ')
   end
@@ -8,7 +8,7 @@ module BooksHelper
   def book_dimensions(book)
     "H: #{book.height}\" x W: #{book.width}\" x D: #{book.depth}\""
   end
-  
+
   def short_book_description(book)
     book.description[0..VISIBLE_LENGTH_DESCRIPTION_BOOK]
   end
