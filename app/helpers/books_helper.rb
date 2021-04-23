ea342c64e22a0c82551fd173f6422fea638b0ded
yar-ua <yar.ua.1985@gmail.com>
2021-04-23 23:21:39 +0300

    add book decorator

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,15 +1,3 @@
 module BooksHelper
-  VISIBLE_LENGTH_DESCRIPTION_BOOK = 100
-
-  def book_authors(book)
-    book.authors.map { |author| "#{author.first_name} #{author.last_name}" }.join(', ')
-  end
-
-  def book_dimensions(book)
-    "H: #{book.height}\" x W: #{book.width}\" x D: #{book.depth}\""
-  end
-
-  def short_book_description(book)
-    book.description[0..VISIBLE_LENGTH_DESCRIPTION_BOOK]
-  end
+  include Pagy::Frontend
 end
