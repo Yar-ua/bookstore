e01cde7abc8f2efccbf3f9129d6056349db3a345
yar-ua <yar.ua.1985@gmail.com>
2021-05-07 10:25:31 +0300

    bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,18 @@ Rails.application.configure do
 
   # Configure ActionMailer
   config.action_mailer.perform_caching = false
-  # config.action_mailer.default_url_options = { host: 'yar-bstore.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'yar-bstore.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.dig(:action_mailer, :user),
+    password: Rails.application.credentials.dig(:action_mailer, :password),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
