dc59a691bb42ccfe1d1bd1c3ad1405b0ac43e291
yar-ua <yar.ua.1985@gmail.com>
2021-05-09 09:41:22 +0300

    bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com', #+
     port: 587, #+
-    domain: 'gmail.com',
+    # domain: 'gmail.com',
     user_name: Rails.application.credentials.dig(:action_mailer, :user), #+
     password: Rails.application.credentials.dig(:action_mailer, :app_password), #+
     authentication: 'plain', #+
