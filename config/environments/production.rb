2507dce3cc2a63e9d87b8d3f1401897322e5608b
yar-ua <yar.ua.1985@gmail.com>
2021-05-09 10:19:37 +0300

    bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,34 +64,22 @@ Rails.application.configure do
 
   # Configure ActionMailer
   config.action_mailer.perform_caching = false
-  # config.action_mailer.default_url_options = { host: 'https://yar-bstore.herokuapp.com/' }
-  # config.action_mailer.delivery_method = :smtp
-    
-  config.action_mailer.default_url_options = { host: 'yar-bstore.herokuapp.com' } #+
-  config.action_mailer.delivery_method = :smtp #+
-  config.action_mailer.perform_deliveries = true #+
-  config.action_mailer.raise_delivery_errors = true #+
-  config.action_mailer.default charset: 'utf-8' #+
+
+  config.action_mailer.default_url_options = { host: 'yar-bstore.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: 'utf-8'
   
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', #+
-    port: 587, #+
+    address: 'smtp.gmail.com',
+    port: 587,
     # domain: 'gmail.com',
-    user_name: Rails.application.credentials.dig(:action_mailer, :user), #+
-    password: Rails.application.credentials.dig(:action_mailer, :app_password), #+
-    authentication: 'plain', #+
-    enable_starttls_auto: true #+
+    user_name: Rails.application.credentials.dig(:action_mailer, :user),
+    password: Rails.application.credentials.dig(:action_mailer, :app_password),
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
-  
-  # config.action_mailer.smtp_settings = {
-  #   user_name: 'apikey',
-  #   password: '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',
-  #   domain: 'yar-bstore.herokuapp.com',
-  #   address: 'smtp.sendgrid.net',
-  #   port: 465,
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
