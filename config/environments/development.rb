1c7d84f5978a2d9d100c179ea3c7a6e5278dbd3b
yar-ua <yar.ua.1985@gmail.com>
2021-05-09 10:32:05 +0300

    final bugfix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    # domain: 'gmail.com',
     user_name: Rails.application.credentials.dig(:action_mailer, :user),
     password: Rails.application.credentials.dig(:action_mailer, :app_password),
     authentication: 'plain',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,17 +64,15 @@ Rails.application.configure do
 
   # Configure ActionMailer
   config.action_mailer.perform_caching = false
-
+  
   config.action_mailer.default_url_options = { host: 'yar-bstore.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
-  
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    # domain: 'gmail.com',
     user_name: Rails.application.credentials.dig(:action_mailer, :user),
     password: Rails.application.credentials.dig(:action_mailer, :app_password),
     authentication: 'plain',
