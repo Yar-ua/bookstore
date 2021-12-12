fcfaf797f19379ddd2dc838003e5981c6693687f
yar-ua <yar.ua.1985@gmail.com>
2021-12-12 14:42:47 +0300

    add authorization during checkout

---
+++ app/controllers/checkouts/addresses_controller.rb
@@ -0,0 +1,10 @@
+module Checkouts
+  class AddressesController < ApplicationController
+    before_action :authenticate_user!
+    
+    def show
+      puts '====addresses controller ===>'
+      render :show
+    end
+  end
+end
